85d7928c18129e38c6b0aaa425f7a5f8ff83b5a1
James Scalise <jamescscalise@gmail.com>
2019-08-13 15:43:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each {|name|
+    messages.push(badge_maker(name))
+  }
+  return messages
+end
+
+def assign_rooms(names)
+  assignments = []
+  names.each_with_index{|name, index|
+    assignments << ("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  }
+  return assignments
+end
+
+def printer(names)
+  
+end
